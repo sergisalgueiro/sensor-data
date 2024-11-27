953a3e14c05f3f6dbd1481584b2f6c874784790e
Sergi Salgueiro Genís <sergi.salgueiro@wallbox.com>
2024-11-27 20:14:47 +0100

    web server 1st attempt

--- webserver/webserver.py
+++ webserver/webserver.py
@@ -1,4 +1,6 @@
 import dotenv
+from flask import Flask, jsonify
+import redis
 import os
 
 load_dotenv()
@@ -6,5 +8,18 @@ load_dotenv()
 REDIS_HOST = os.getenv("REDIS_HOST")
 REDIS_PORT = os.getenv("REDIS_PORT")
 
-if __name__ == "__main__":
-    pass
+app = Flask(__name__)
+
+# Configure Redis
+redis_client = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=0)
+
+@app.route('/')
+def index():
+    # Fetch all keys (topics) and their latest values
+    sensor_data = {}
+    for key in redis_client.keys():
+        sensor_data[key.decode()] = redis_client.get(key).decode()
+    return jsonify(sensor_data)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
