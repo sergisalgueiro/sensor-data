28c74d4188b18121eb261f63483437114b42482b
Sergi Salgueiro Genís <sergi.salgueiro@wallbox.com>
2024-11-27 21:59:57 +0100

    html ccs

--- webserver/webserver.py
+++ webserver/webserver.py
@@ -1,5 +1,5 @@
 from dotenv import load_dotenv
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 import redis
 import os
 
@@ -16,10 +16,8 @@ redis_client = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=0)
 @app.route('/')
 def index():
     # Fetch all keys (topics) matching temp_hum_sensor_* and their latest values
-    sensor_data = {}
-    for key in redis_client.keys("temp_hum_sensor_*"):
-        sensor_data[key.decode()] = redis_client.get(key).decode()
-    return jsonify(sensor_data)
+    sensor_data = {key.decode(): redis_client.get(key).decode() for key in redis_client.keys("temp_hum_sensor_*")}
+    return render_template('index.html', sensor_data=sensor_data)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
