1e295287d3ceab8c313e80786d191e2a9ef1fa9e
Sergi Salgueiro Genís <sergi.salgueiro@wallbox.com>
2024-12-02 09:50:15 +0100

    lower httpx verbosity

--- telegram-bot/telegrambot.py
+++ telegram-bot/telegrambot.py
@@ -10,6 +10,7 @@ from common.MQTTClient import MQTTClient
 
 # Set up logger
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logging.getLogger("httpx").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
 load_dotenv()
